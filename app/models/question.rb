edc05934d87d017c74c8f1883a23dfa239ab56c5
takumi19910112 <114665284+takumi19910112@users.noreply.github.com>
2022-11-10 22:46:31 +0900

    バリデーションの修正

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :tag
   validates :content,:title,:images, presence: true
-  validates :genre_id, numericality: { other_than: 1 , message: "タグを選択してください"}
+  validates :tag_id, numericality: { other_than: 1 , message: "タグを選択してください"}
 
 
   belongs_to :user
