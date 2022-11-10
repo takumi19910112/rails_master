18ae31b5ba82dd3f53e9bedf192a006451ce1357
takumi19910112 <114665284+takumi19910112@users.noreply.github.com>
2022-11-10 22:04:54 +0900

    新規投稿ページの作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,26 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.includes(:user).order("created_at DESC")
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:tag_id,:title,:content,).merge(user_id: current_user.id)
+  end
+
+
 end
