343216cd2a360ce19a43b40c6fcf792312a77292
takumi19910112 <114665284+takumi19910112@users.noreply.github.com>
2022-11-11 09:33:34 +0900

    prototypeから変更点を移植

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,8 +13,38 @@ class QuestionsController < ApplicationController
       redirect_to root_path
     else
       render :new
-    end
   end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+    prototype = Prototype.find(params[:id])
+  end
+  
+  def edit
+    @prototype = Prototype.find(params[:id])
+     unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to action: :index
+     end 
+  end
+  
+ def update
+    @prototype = Prototype.find(params[:id])
+     if @prototype.update(prototype_params)
+      redirect_to prototype_path
+     else
+      render :edit
+      end
+ end
+  
+ def destroy
+     prototype = Prototype.find(params[:id])
+     prototype.destroy
+     redirect_to root_path
+ end
+
+end
 
   private
 
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :tag
-  validates :content,:title,:images, presence: true
+  validates :content,:title, presence: true
   validates :tag_id, numericality: { other_than: 1 , message: "タグを選択してください"}
 
 
