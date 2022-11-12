d1fef70f2c665ac98edd868155a8e261118663fc
takumi19910112 <114665284+takumi19910112@users.noreply.github.com>
2022-11-12 22:00:18 +0900

    お気に入り機能の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @questions = @user.questions.order("created_at DESC")
-     favorites = Favorite.where(user_id: current_user.id).pluck(:question_id) 
-     @favorite_list = Question.find(favorites) 
+    favorites = Favorite.where(user_id: @user.id).pluck(:question_id) 
+    @favorite_list = Question.find(favorites) 
   end   
 end
