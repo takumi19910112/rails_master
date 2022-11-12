8d74b22199cc1c223e872e416720fb4f4dcc2304
takumi19910112 <114665284+takumi19910112@users.noreply.github.com>
2022-11-12 21:33:03 +0900

    見た目の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @questions = @user.questions
+    @questions = @user.questions.order("created_at DESC")
 
     # favorites = Favorite.where(user_id: current_user.id).pluck(:question_id) 
     # @favorite_list = Question.find(favorites) 
