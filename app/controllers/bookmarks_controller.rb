78aee73fe041ce8603fa48fbfbb50fcd47534e50
takumi19910112 <114665284+takumi19910112@users.noreply.github.com>
2022-11-16 03:08:33 +0900

    ブックマーク機能完成

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,25 +1,16 @@
   class BookmarksController < ApplicationController
-    before_action :authenticate_user!
-  
     def create
       @question = Question.find(params[:question_id])
       bookmark = @question.bookmarks.new(user_id: current_user.id)
-      if bookmark.save
-        redirect_to request.referer
-      else
-        redirect_to request.referer
-      end
+      bookmark.save
+      flash[:success] = "Liked post"
+      redirect_to request.referer
     end
   
     def destroy
       @question = Question.find(params[:question_id])
-      bookmark = @post.bookmarks.find_by(user_id: current_user.id)
-      if bookmark.present?
-          bookmark.destroy
-          redirect_to request.referer
-      else
-          redirect_to request.referer
-      end
+      bookmark = current_user.bookmarks.find_by(question_id: @question.id)
+      bookmark.destroy
+      redirect_to request.referer
     end
-  end
 end
